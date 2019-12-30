e12f8685d58c775fce5848f31d6371eae054954b
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-12-30 14:17:17 -0500

    Create localStorage.js

---
+++ 15 - LocalStorage/localStorage.js
@@ -0,0 +1,11 @@
+//? logs timestamp of refresh
+//*--------------------------------------------------------
+const compiled = new Date();
+console.info("compiled: ", compiled);
+
+//*-------------------------------------------------------- //*--------------------------------------------------------
+//*-------------------------------------------------------- //*--------------------------------------------------------
+
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = [];
