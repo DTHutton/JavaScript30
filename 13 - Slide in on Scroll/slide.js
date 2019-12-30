5f4e7e989476259939e9597ef9131086cafec92e
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-12-30 12:52:04 -0500

    Update slide.js

--- 13 - Slide in on Scroll/slide.js
+++ 13 - Slide in on Scroll/slide.js
@@ -1,4 +1,4 @@
-//? logs timestamp of reload
+//? logs timestamp of refresh
 //*--------------------------------------------------------
 const compiled = new Date();
 console.info("compiled: ", compiled);
@@ -7,16 +7,46 @@ console.info("compiled: ", compiled);
 //*-------------------------------------------------------- //*--------------------------------------------------------
 
 function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function () {
-        var context = this, args = arguments;
-        var later = function () {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
+	let timeout;
+	return function () {
+		const context = this,
+			args = arguments;
+		const later = function () {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		const callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
 }
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+function checkSlide(e) {
+	sliderImages.forEach(sliderImage => {
+		//! halfway through the image
+		const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+		// console.log('slideInAt', slideInAt);
+		//! bottom of the image
+		const imageBottom = sliderImage.offsetTop + sliderImage.height;
+		// console.log('imageBottom', imageBottom);
+		//! checks if the image is showing half of it's height
+		const isHalfShown = slideInAt > sliderImage.offsetTop;
+		// console.log('isHalfShown', isHalfShown);
+		//! checks if the image has been scolled past
+		const isNotScrolledPast = window.scrollY < imageBottom;
+		// console.log('isNotScrolledPast', isNotScrolledPast);
+		//! if half the image is showing and the image has not been scrolled past:
+		//! `true` - adds '.active'
+		//! `false` - removes '.active'
+		if (isHalfShown && isNotScrolledPast) {
+			sliderImage.classList.add('active');
+		} else {
+			sliderImage.classList.remove('active');
+		}
+	});
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
