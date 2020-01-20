d86b7660a547d7b75f7180b027c97ec9bb1769be
DTHutton <48737212+DTHutton@users.noreply.github.com>
2020-01-20 10:19:18 -0500

    Create stickyNav.js

---
+++ 24 - Sticky Nav/stickyNav.js
@@ -0,0 +1,17 @@
+console.info('compiled: ', new Date()) //? logs timestamp of refresh
+//*--------------------------------------------------------------------
+
+const nav = document.querySelector('#main')
+const topOfNav = nav.offsetTop
+
+function fixNav() {
+	if (window.scrollY >= topOfNav) {
+		document.body.style.paddingTop = nav.offsetHeight + 'px'
+		document.body.classList.add('fixed-nav')
+	} else {
+		document.body.style.paddingTop = 0
+		document.body.classList.remove('fixed-nav')
+	}
+}
+
+window.addEventListener('scroll', fixNav)
